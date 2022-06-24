731bb90096dc27b442a501d1e8c9c29695addc20
Faber Cardenas <fabercardenas@gmail.com>
2022-06-24 15:32:42 -0500

    inicio pys

---
+++ Datos/DPYS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Collections;
+
+namespace Datos
+{
+    public class DPYS : DSQL
+    {
+		public DataTable PorSincronizar()
+		{
+			return procedureTable("tdcPorSincronizar", false);
+		}
+
+		public DataTable ConsultarPazySalvoXemitir()
+		{
+			Hashtable param = new Hashtable(1);
+			return procedureTable("pysConsultarPazySalvoXemitir", true, param);
+		}
+	}
+}
