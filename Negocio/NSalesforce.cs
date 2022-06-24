26df5a3f4913be20b1c64ed823107e7b0c34a2c4
Faber Cardenas <fcardenas@qnt.com.co>
2022-06-24 15:17:17 -0500

    variables sf para produccion

--- Negocio/NSalesforce.cs
+++ Negocio/NSalesforce.cs
@@ -19,10 +19,10 @@ namespace Negocio
             HttpContent content = new FormUrlEncodedContent(new Dictionary<string, string>
             {
                 {"grant_type", "password"},
-                {"client_id", "3MVG9rrOVHA54N6PJLV78rRcYv.rBxNgKHPHKiLvb6pWsS5A4KJ9uK72AAQ0wyivNuBG3s8VeOdmKyRtpp_nq"},
-                {"client_secret", "F239BA4172C95CD901899D84ED9C52685631ADF73805C96718CC03BD0FCC248A"},
-                {"username", "agomez@qnt.com.co.uat"},
-                {"password", "1qazse4rfvgy7axzvfGgGLxj13rGXBLn3LnlaX"}
+                {"client_id", ConfigurationManager.AppSettings["sfClientId"].ToString()},
+                {"client_secret", ConfigurationManager.AppSettings["sfSecret"].ToString()},
+                {"username", ConfigurationManager.AppSettings["sfUser"].ToString()},
+                {"password", ConfigurationManager.AppSettings["sfPass"].ToString()}
             });
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
             string LoginEndpoint = ConfigurationManager.AppSettings["sfEndpoint"].ToString();
