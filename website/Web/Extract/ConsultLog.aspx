d65ef2dbfe22e3a4073ed671e5cddbbd0d8b093a
Faber Cardenas <fcardenas@qnt.com.co>
2022-07-15 19:32:49 -0500

    extracto consulta

---
+++ website/Web/Extract/ConsultLog.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Web/MasterPage.master" AutoEventWireup="true" CodeFile="ConsultLog.aspx.cs" Inherits="CosultLog" MaintainScrollPositionOnPostback="true" %>
+<%@ Register assembly="AjaxControlToolkit" namespace="AjaxControlToolkit" tagprefix="asp" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+<div class="clearfix"></div>
+
+<asp:Literal ID="ltrMensaje" runat="server"></asp:Literal>
+
+<asp:Panel ID="pnlInicial" runat="server">
+    <div id="dvIdConsulta" runat="server">
+        <%--Tarjeta--%>
+        <div class="x_panel">
+            <div class="row" >
+                <div class="col-md-2" style="margin-top:5px;">
+                    Documento del cliente
+                </div>
+                <div class="col-md-2">
+                    <asp:TextBox ID="txtNumDocumento" runat="server" CssClass="form-control" ValidationGroup="principal"></asp:TextBox>
+                    <asp:FilteredTextBoxExtender ID="fteNumTarjeta" runat="server" TargetControlID="txtNumDocumento" FilterType="Numbers"></asp:FilteredTextBoxExtender>
+                    <asp:RequiredFieldValidator ID="rfvNumTarjeta" runat="server" ControlToValidate="txtNumDocumento" ErrorMessage="* Campo Requerido" ValidationGroup="principal"> </asp:RequiredFieldValidator>
+                </div>
+                <div class="col-md-5 text-left">
+                     <asp:LinkButton ID="lnbConsultarDocumento" runat="server" CssClass="btn btn-primary" OnClick="lnbConsultarDocumento_Click" ValidationGroup="principal">
+                        <span class="fa fa-send"></span>
+                        &nbsp;&nbsp;Consultar
+                    </asp:LinkButton>
+                </div>
+                
+            </div>
+            <div style="margin-top:-10px; border-top:2px solid #E6E9ED;">
+            </div> 
+            <div class="clearfix"></div>
+            <div class="x_content">
+            <br />
+            aqui
+            <asp:GridView ID="gdvListaSolicitudes" runat="server" CssClass="table table-hover" 
+                EmptyDataText="No hay solicitues con los filtros seleccionados" 
+                    AutoGenerateColumns="False" Width="100%"
+                    AllowPaging="true" PageSize="30" GridLines="None"
+                    DataKeyNames="tdc_numeroDocumento, tdc_docIngresos, tdc_docIdentidad"
+                    OnRowDataBound="gdvListaSolicitudes_RowDataBound" OnRowCommand="gdvListaSolicitudes_RowCommand"
+                    >
+                <Columns>
+                    <asp:BoundField DataField="tdc_numeroDocumento" HeaderText="Documento" ItemStyle-HorizontalAlign="Center" />
+                    <asp:BoundField DataField="tdc_numeroTarjeta" HeaderText="No. Tarjeta" ItemStyle-HorizontalAlign="Center"  />
+                    <asp:BoundField DataField="Nombre" HeaderText="Nombres y Apellidos" ItemStyle-HorizontalAlign="Left" />
+                    <asp:TemplateField HeaderText="Flujo Digital" ItemStyle-HorizontalAlign="Center">
+                        <ItemTemplate>
+                            <%# TieneFlujoDigital((Boolean)Eval("tdc_flujoDigital")) %>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Pagaré" ItemStyle-HorizontalAlign="Center">
+                        <ItemTemplate>
+                            <%# TienePagare((Boolean)Eval("tdc_docPagare")) %>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Doc. Identidad" ItemStyle-HorizontalAlign="Center">
+                        <ItemTemplate>
+                            <asp:LinkButton  ID="lbtDocIdentidad" runat="server" CommandName="DocIdentidadRecibir" CommandArgument="<%# Container.DataItemIndex %>"
+                                  Text="Recibido"/>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Soporte Ingresos" ItemStyle-HorizontalAlign="Center">
+                        <ItemTemplate>
+                            <asp:LinkButton  ID="lbtSopIngresos" runat="server" CommandName="SopIngresosRecibir" CommandArgument="<%# Container.DataItemIndex %>"
+                                  Text="Recibido"/>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
+        </div> 
+    </div>
+
+</asp:Panel>
+
+</asp:Content>
---
+++ website/Web/Extract/ConsultLog.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Web.UI.HtmlControls;
+using System.Data.OleDb;
+using Ionic.Zip;
+using OfficeOpenXml;
+
+public partial class CosultLog : System.Web.UI.Page
+{
+    Negocio.NTDC nTDC = new Negocio.NTDC();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if ((Session["ID_usuario"] != null) && (!Page.IsPostBack))
+        {
+            Literal ltrTituloModulo = (Literal)this.Master.FindControl("ltrTituloModulo");
+            ltrTituloModulo.Text = "<span class='fa fa-send'></span> Consultar Log de Envio de Extractos";
+            dvIdConsulta.Visible = true;
+        }
+    }
+
+
+    protected void lnbConsultarDocumento_Click(object sender, EventArgs e)
+    {
+        ConsultarSolicitudesXdocumento();
+    }
+
+    void ConsultarDocumental()
+    {
+        gdvListaSolicitudes.DataSource = nTDC.ConsultarDocumental();
+        gdvListaSolicitudes.DataBind();
+        
+    }
+
+    public string TieneFlujoDigital(Boolean tdc_flujoDigital)
+    {
+        string retornar = "";
+        if (tdc_flujoDigital)
+            retornar = "<span class='glyphicon glyphicon-ok'></span>";
+        return retornar;
+    }
+
+    public string TienePagare(Boolean tdc_docPagare)
+    {
+        string retornar = "";
+        if (tdc_docPagare)
+            retornar = "<span class='glyphicon glyphicon-ok'></span>";
+        return retornar;
+    }
+
+    void ConsultarSolicitudesXdocumento()
+    {
+        gdvListaSolicitudes.DataSource = nTDC.consultaSolicitudXDocumento(txtNumDocumento.Text);
+        gdvListaSolicitudes.DataBind();
+    }
+
+    protected void lnbGenerarSMS_Click(object sender, EventArgs e)
+    {
+        string popupScript = "<script language='JavaScript'>" +
+                             "window.open('FileSMS.aspx','SMS', 'width=400, height=80,location=no, menubar=no,status=no,scrollbars=no,target=blank')</script>";
+        ClientScript.RegisterStartupScript(this.GetType(), "SMS", popupScript);
+    }
+
+    protected void gdvListaSolicitudes_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            Boolean docIngresos = (Boolean)gdvListaSolicitudes.DataKeys[e.Row.DataItemIndex].Values[1];
+            LinkButton buttonSopIngresos = (LinkButton)e.Row.Cells[6].FindControl("lbtSopIngresos");
+
+            Boolean docIdentidad = (Boolean)gdvListaSolicitudes.DataKeys[e.Row.DataItemIndex].Values[2];
+            LinkButton buttonDocIdentidad = (LinkButton)e.Row.Cells[5].FindControl("lbtDocIdentidad");
+
+            if (buttonSopIngresos != null)
+            {
+                if (docIngresos)
+                {
+                    buttonSopIngresos.Text = "<span class='glyphicon glyphicon-ok'></span>";
+                    buttonSopIngresos.Enabled = false;
+                }
+                else
+                {
+                    buttonSopIngresos.Text = "<span class='btn btn-xs btn-success'>Recibido</span>";
+                }
+            }
+
+            if (buttonDocIdentidad != null)
+            {
+                if (docIdentidad)
+                {
+                    buttonDocIdentidad.Text = "<span class='glyphicon glyphicon-ok'></span>";
+                    buttonDocIdentidad.Enabled = false;
+                }
+                else
+                {
+                    buttonDocIdentidad.Text = "<span class='btn btn-xs btn-success'>Recibido</span>";
+                }
+            }
+        }
+    }
+
+    protected void gdvListaSolicitudes_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        if (e.CommandName == "SopIngresosRecibir")
+        {
+            int index = Convert.ToInt32(e.CommandArgument);
+            string ltrCliDocumento = gdvListaSolicitudes.DataKeys[index].Values[0].ToString();
+            nTDC.actualizaDocIngresosXDocumento(ltrCliDocumento, Session["ID_usuario"].ToString());
+            ltrMensaje.Text = Messaging.Success("Soporte de Ingresos para el Documento " + ltrCliDocumento + " actualizado con éxito");
+            ConsultarDocumental();
+        }
+
+        if (e.CommandName == "DocIdentidadRecibir")
+        {
+            int index = Convert.ToInt32(e.CommandArgument);
+            string ltrCliDocumento = gdvListaSolicitudes.DataKeys[index].Values[0].ToString();
+            nTDC.actualizaDocIdentidadXDocumento(ltrCliDocumento, Session["ID_usuario"].ToString());
+            ltrMensaje.Text = Messaging.Success("Documento de Identidad para el Documento " + ltrCliDocumento + " actualizado con éxito");
+            ConsultarDocumental();
+        }
+    }
+}
